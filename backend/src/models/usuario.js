a414e080914201fe74691395a38098fe89126dea
Joao Pedro <joaopedropavanelo@gmail.com>
2026-05-26 22:49:24 -0300

    mudança no usuarioSchema

--- backend/src/models/usuario.js
+++ backend/src/models/usuario.js
@@ -5,9 +5,25 @@ const usuarioSchema = new mongoose.Schema({
         type: Number,
         unique: true
     },
-    nome: String,
-    email: String,
-    senha: String
+    nome: {
+        type: String,
+        required: [true, "Nome obrigatorio!"],
+        trim: true,
+        minlength: [3, "O nome deve conter no mínimo 3 caracteres!"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email obrigatorio!"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, "Email inválido!"]
+    },
+    senha: {
+        type: String,
+        required: [true, "Senha obrigatoria!"],
+        minlength: [4, "Deve conter no minimo 4 caracteres!"]
+    }
 });
 
 const Usuario = mongoose.models.Usuario || mongoose.model("Usuario", usuarioSchema);
